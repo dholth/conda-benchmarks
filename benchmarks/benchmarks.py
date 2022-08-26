cee8bf974ebc27de8d158b1d05eeea170ac27c72
Daniel Holth <dholth@anaconda.com>
2022-08-26 09:50:10 -0400

    time package-handling vs package-streaming

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,12 +1,18 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
+import pathlib
+
+from conda_package_handling.api import extract
+from conda_package_streaming import package_streaming
+
 
 class TimeSuite:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+
     def setup(self):
         self.d = {}
         for x in range(500):
@@ -24,6 +30,47 @@ class TimeSuite:
         d = self.d
         for key in range(500):
             x = d[key]
+
+
+class TimeCondaPackageHandling:
+    def setup(self):
+        self.condas = list(
+            pathlib.Path("~/miniconda3/pkgs").expanduser().glob("*.conda")
+        )
+
+    def time_cph(self):
+        """
+        Extract a bunch of .conda with conda-package-handling
+        """
+        base = pathlib.Path("/tmp/condas-cph")
+        base.mkdir(exist_ok=True)
+        for conda in self.condas:
+            stem = conda.stem
+            dest_dir = base / stem
+            dest_dir.mkdir(exist_ok=True)
+            extract(str(conda), dest_dir)
+
+    def time_cps(self):
+        """
+        Extract a bunch of .conda with conda-package-streaming
+        """
+        base = pathlib.Path("/tmp/condas-cps")
+        base.mkdir(exist_ok=True)
+        for conda in self.condas:
+            stem = conda.stem
+            dest_dir = base / stem
+            dest_dir.mkdir(exist_ok=True)
+            with conda.open(mode="rb") as fp:
+                for tar, member in package_streaming.stream_conda_component(
+                    str(conda), fp, component="info"
+                ):
+                    tar.extractall(dest_dir)
+                    break
+                for tar, member in package_streaming.stream_conda_component(
+                    str(conda), fp, component="pkg"
+                ):
+                    tar.extractall(dest_dir)
+                    break
 
 
 class MemSuite:

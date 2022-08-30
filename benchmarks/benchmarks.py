ec3dd91c684dba1e45e1d49500202eb765fcb567
Daniel Holth <dholth@anaconda.com>
2022-08-30 13:42:57 -0400

    track handling / streaming as a ratio

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -2,77 +2,135 @@
 # See "Writing benchmarks" in the asv docs for more information.
 
 import pathlib
+from tempfile import TemporaryDirectory
+import time
+from tkinter.tix import MAX
 
 from conda_package_handling.api import extract
 from conda_package_streaming import package_streaming
 
 
-class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
-
-    def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
-
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_values(self):
-        for value in self.d.values():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
+MINIMUM_PACKAGES = 10
+MAXIMUM_SECONDS = 8.0
 
 
-class TimeCondaPackageHandling:
+class TrackSuite:
     def setup(self):
         self.condas = list(
             pathlib.Path("~/miniconda3/pkgs").expanduser().glob("*.conda")
         )
+        if len(self.condas) < MINIMUM_PACKAGES:
+            raise NotImplementedError("Not enough .conda packages in ~/miniconda3/pkgs")
 
-    def time_cph(self):
-        """
-        Extract a bunch of .conda with conda-package-handling
-        """
-        base = pathlib.Path("/tmp/condas-cph")
-        base.mkdir(exist_ok=True)
-        for conda in self.condas:
-            stem = conda.stem
-            dest_dir = base / stem
-            dest_dir.mkdir(exist_ok=True)
-            extract(str(conda), dest_dir)
+        self.tarbz2 = list(
+            pathlib.Path("~/miniconda3/pkgs").expanduser().glob("*.tar.bz2")
+        )
 
-    def time_cps(self):
+    def track_streaming_versus_handling(self):
         """
-        Extract a bunch of .conda with conda-package-streaming
+        Compare conda-package-streaming time versus conda-package-handling (should be a number > 1)
         """
-        base = pathlib.Path("/tmp/condas-cps")
-        base.mkdir(exist_ok=True)
-        for conda in self.condas:
-            stem = conda.stem
-            dest_dir = base / stem
-            dest_dir.mkdir(exist_ok=True)
-            with conda.open(mode="rb") as fp:
-                for tar, member in package_streaming.stream_conda_component(
-                    str(conda), fp, component="info"
-                ):
-                    tar.extractall(dest_dir)
-                    break
-                for tar, member in package_streaming.stream_conda_component(
-                    str(conda), fp, component="pkg"
-                ):
-                    tar.extractall(dest_dir)
-                    break
+        attempted = len(self.condas)
+        with TemporaryDirectory(
+            "conda-package-streaming"
+        ) as streaming, TemporaryDirectory("conda-package-handling") as handling:
+
+            # give faster streaming the cache disadvantage
+            begin = time.monotonic()
+            # revise self.condas down to the number extracted in no more thanœ
+            # MAXIMUM_SECONDS
+            self.condas = extract_streaming(streaming, self.condas)
+            end = time.monotonic()
+            cps_time = end - begin
+
+            actual = len(self.condas)
+            print(f"'streaming' extracted {actual} out of {attempted} .conda's")
+
+            begin = time.monotonic()
+            self.condas = extract_handling(handling, self.condas, time_limit=cps_time * 4)
+            end = time.monotonic()
+            handling_time = end - begin
+
+            actual = len(self.condas)
+            print(f"'handling' extracted {actual} out of {attempted} .conda's")
+
+            return handling_time / cps_time
 
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    def track_streaming_versus_handling_tarbz2(self):
+        """
+        Compare conda-package-streaming time versus conda-package-handling (should be a number > 1)
+        """
+        attempted = len(self.condas)
+        with TemporaryDirectory(
+            "conda-package-streaming-bz2"
+        ) as streaming, TemporaryDirectory("conda-package-handling-bz2") as handling:
+
+            # give faster streaming the cache disadvantage
+            begin = time.monotonic()
+            # revise self.condas down to the number extracted in no more thanœ
+            # MAXIMUM_SECONDS
+            self.tarbz2 = extract_streaming(streaming, self.tarbz2)
+            end = time.monotonic()
+            cps_time = end - begin
+
+            actual = len(self.condas)
+            print(f"'streaming' extracted {actual} out of {attempted} .tar.bz2's")
+
+            begin = time.monotonic()
+            self.tarbz2 = extract_handling(handling, self.tarbz2, time_limit=cps_time * 4)
+            end = time.monotonic()
+            handling_time = end - begin
+
+            actual = len(self.condas)
+            print(f"'handling' extracted {actual} out of {attempted} .tar.bz2's")
+
+            return handling_time / cps_time
+
+def extract_handling(base, condas, time_limit=MAXIMUM_SECONDS):
+    """
+    Extract a bunch of .conda with conda-package-handling
+    """
+    limit = time.monotonic()
+    extracted = []
+    base = pathlib.Path(base)
+    for conda in condas:
+        stem = conda.stem
+        dest_dir = base / stem
+        dest_dir.mkdir(exist_ok=True)
+        extract(str(conda), dest_dir)
+        extracted.append(conda)
+        if time.monotonic() - limit > time_limit:
+            break
+
+    return extracted
+
+def extract_streaming(base, condas, time_limit=MAXIMUM_SECONDS):
+    """
+    Extract a bunch of .conda with conda-package-streaming
+    """
+    limit = time.monotonic()
+    extracted = []
+    base = pathlib.Path(base)
+    for conda in condas:
+        stem = conda.stem
+        dest_dir = base / stem
+        dest_dir.mkdir(exist_ok=True)
+        with conda.open(mode="rb") as fp:
+            for tar, _member in package_streaming.stream_conda_component(
+                str(conda), fp, component="info"
+            ):
+                tar.extractall(dest_dir)
+                break
+            if conda.name.endswith(".tar.bz2"):
+                break  # .tar.bz2 will be totally extracted, doesn't filter by component here
+            for tar, _member in package_streaming.stream_conda_component(
+                str(conda), fp, component="pkg"
+            ):
+                tar.extractall(dest_dir)
+                break
+        extracted.append(conda)
+        if time.monotonic() - limit > time_limit:
+            break
+
+    return extracted
